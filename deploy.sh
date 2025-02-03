781b3904fce063ab7c962e90236e4c67716c4b7a
Prabhat Agarwal <prabhat.agr2010@gmail.com>
2025-02-03 09:56:28 +0000

    Pending changes exported from your codespace

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set +ex
 docker build -t gcr.io/premium-state-449406-n8/planet-demo:v1 -f docker/Dockerfile .
 docker push gcr.io/premium-state-449406-n8/planet-demo:v1
 
-gcloud run deploy streamlit-app \
+gcloud run deploy planet-stanford \
     --image gcr.io/premium-state-449406-n8/planet-demo:v1 \
     --platform managed \
     --region us-east1 \
