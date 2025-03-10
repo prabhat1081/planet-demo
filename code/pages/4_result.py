f29c588e93d920b59d6d94218d020c11c6639b7f
Michihiro Yasunaga <myasu@cs.stanford.edu>
2025-03-10 13:23:26 -0700

    update result page

--- code/pages/4_result.py
+++ code/pages/4_result.py
@@ -6,6 +6,29 @@ from app.common import layout_trial_data
 # Initialize GCS client
 storage_client = storage.Client()
 
+def get_trial_data(request_id):
+    """Retrieves trial data from GCS given a request ID."""
+    bucket_name = "planet-stanford"  # Your GCS bucket name
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(f"{request_id}.json")
+
+    try:
+        data = json.loads(blob.download_as_bytes())
+        return data
+    except Exception as e:
+        st.error(f"Error retrieving data for Request ID {request_id}: {e}")
+        return None
+
+
+def display_trial_data(request_id):
+    trial_data = get_trial_data(request_id)
+    if trial_data:
+        st.write("Trial Data Details:")
+        layout_trial_data(trial_data)
+    else:
+        st.write(f"No data found for request ID: {request_id}")
+
+
 def get_result_data(request_id):
     """Retrieves trial data from GCS given a request ID."""
     bucket_name = "planet-stanford"  # Your GCS bucket name
@@ -27,6 +50,7 @@ def layout_result_data(result_data):
         return
 
     st.subheader("Adverse Events")
+    st.write(f"**Top 5 adverse events with the highest probability:**")
     aes = result_data["Top adverse events predicted for trial 1"]
     for ae_name, score in aes.items():
         st.write(f"**{ae_name}:** {score:0.3f}")
@@ -53,9 +77,11 @@ st.title("Retrieve Results for Request ID")
 request_id_from_url = st.query_params.get("id", None) #Get id from query params
 
 if request_id_from_url: #If id is in url
+    display_trial_data(request_id_from_url)
     display_result_data(request_id_from_url)
 else: #If id is not in url
     request_id_input = st.text_input("Enter Request ID") #Allow user to input request id
     if request_id_input: #If user inputted request id
+        display_trial_data(request_id_input)
         display_result_data(request_id_input)
 
